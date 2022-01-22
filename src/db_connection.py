5b1d1f8818ee5f41f38a1c42349e7d9827e26d6d
konfiot <arthur.toussaint@polytechnique.edu>
2022-01-22 12:20:39 +0100

    Update path

--- src/db_connection.py
+++ src/db_connection.py
@@ -8,15 +8,20 @@ home_path = os.path.expanduser('~')
 
 fileName = 'X509-cert-999459858208805076.pem' # CHANGE
 
+path_to_certificate = os.path.join(home_path,'.ssh')
+try:
+    os.mkdir(path_to_certificate)
+except OSError as error:
+    print(error)    
+path_to_certificate = os.path.join(path_to_certificate,fileName)
+
 if os.getenv('MONGODB_CERT', None):
-    with open(fileName, 'w') as f:
+    with open(path_to_certificate, 'w') as f:
         f.write(os.getenv("MONGODB_CERT", None))
         print("file written")
 else:
     print(os.getenv("MONGODB_CERT", None))
 
-path_to_certificate = os.path.join(home_path,'.ssh')
-path_to_certificate = os.path.join(path_to_certificate,fileName)
 
 uri = "mongodb+srv://cluster0.zdfyr.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
 client = MongoClient(uri,
