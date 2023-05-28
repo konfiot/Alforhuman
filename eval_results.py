f9534e42a2dc38f670a12c69397f015e33576d6f
floregol <florence.robert-regol@mail.mcgill.ca>
2023-05-28 17:08:33 -0400

    oups

--- eval_results.py
+++ eval_results.py
@@ -2,14 +2,8 @@ from src.db_connection import get_all_completed_experiment
 from src.experiment import ExperimentDB
 import numpy as np 
 from scipy.stats import fisher_exact, ttest_ind
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import seaborn as sns
-=======
-
-from vis import look_at_bunch_of_samples
-
->>>>>>> 9e1b3dc6b6eac26c881133743ea3083f757e570b
 
 def extract_score(exp):
     pred_tuple = exp.list_human_pred_test
